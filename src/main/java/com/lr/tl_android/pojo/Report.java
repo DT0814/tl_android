c90dcc6339e9311481f745b78432813f4ed7d9d3
DT0814 <729742011@qq.com>
2018-09-26 16:47:40 +0800

    检举实体创建

---
+++ src/main/java/com/lr/tl_android/pojo/Report.java
@@ -0,0 +1,56 @@
+package com.lr.tl_android.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tl_report")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Report {
+    @Id
+    @GeneratedValue
+    private Integer rid;
+    private Integer uid;
+    private String imagePath;
+    //举报处理状态1未处理2处理完成
+    private byte state;
+
+    public Report() {
+    }
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public byte getState() {
+        return state;
+    }
+
+    public void setState(byte state) {
+        this.state = state;
+    }
+}
