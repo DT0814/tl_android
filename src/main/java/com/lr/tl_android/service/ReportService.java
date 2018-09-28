4864fe28b86c6292abdccaa59f41610f7338ebb0
Jiaqi <pranay_hu@foxmail.com>
2018-09-28 10:54:15 +0800

    upload image

--- src/main/java/com/lr/tl_android/service/ReportService.java
+++ src/main/java/com/lr/tl_android/service/ReportService.java
@@ -4,41 +4,43 @@ import com.lr.tl_android.dao.ImageUploadDao;
 import com.lr.tl_android.pojo.Report;
 import com.lr.tl_android.pojo.result.ReportResult;
 import com.lr.tl_android.utils.ResultCode;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class ReportService {
 
-    @Value("${image.dir}")
-    private String uploadDir;
+    private final static String uploadPath = "../images/";
 
     private ImageUploadDao dao;
+
 
     public ReportResult saveImage(MultipartFile file, Integer uid) throws IOException{
 
         String fileName = file.getOriginalFilename();
         String suffixName = fileName.substring(fileName.lastIndexOf("."));
-        String full_address = file + suffixName;
-        String filePath = uploadDir;
-        File dest = new File(filePath + fileName);
+        fileName = UUID.randomUUID() + suffixName;
+        String filePath = uploadPath + fileName;
 
-        if (!dest.getParentFile().exists()) {
-            dest.getParentFile().mkdirs();
+        File saveFile = new File(filePath);
+        if(!saveFile.getParentFile().exists()) {
+            saveFile.getParentFile().mkdirs();
         }
 
-        try {
-            file.transferTo(dest);
-            Report report = new Report(uid, full_address, false);
-            dao.saveAndFlush(report);
-            return ReportResult.getInstance(ResultCode.SUCCESS);
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return ReportResult.getInstance(ResultCode.IMAGE_UPLOAD_FAIL);
+        OutputStream outputStream = new FileOutputStream(saveFile);
+        IOUtils.copy(file.getInputStream(), outputStream);
+
+        Report report = new Report(uid, filePath, false);
+        dao.saveAndFlush(report);
+
+        return ReportResult.getInstance(ResultCode.SUCCESS);
+
     }
 }
